60fe68217b838f96e72df1855043c9d2ecd1060f
Bruka25 <bkteshome862@gmail.com>
2023-11-26 01:40:23 +0300

    header file for rsa factorization

---
+++ bk_factor.h
@@ -0,0 +1,14 @@
+#ifndef _FACTOR_H_
+#define _FACTOR_H_
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+#define _GNU_SOURCE
+
+
+int bk_factorize(char *buffer);
+
+#endif /* _FACTOR_H_ */
